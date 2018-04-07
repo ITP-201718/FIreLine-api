ee453fd31938ebba671d156c37b300000160131d
David Langheiter <david@langheiter.com>
2018-04-08 00:09:02 +0200

    Added config_example.js

---
+++ node/config_example.js
@@ -0,0 +1,18 @@
+const config = {
+    mysql: {
+        host: '',
+        database: '',
+        user: '',
+        password: '',
+        port: 3306,
+    },
+    wamp: {
+        url: '',
+        realm: '',
+        user: '',
+        password: '',
+        base_uri: ''
+    },
+}
+
+module.exports = config
